d6e3a789a6d847a3f07706019734756b1e85176c
yujoon0214 <130519084+yujoon0214@users.noreply.github.com>
2023-11-10 20:01:12 +0900

    Add files via upload

--- C_Project_11/C_Project_11/C_Project_11.cpp
+++ C_Project_11/C_Project_11/C_Project_11.cpp
@@ -6,37 +6,126 @@
 #include <malloc.h>
 #include <windows.h>
 
-// C언어 파일 입출력
-#define _CRT_SECURE_NO_WARNINGS
+
 
 int main()
 {
-	FILE *fp;
-	int num1 = 0, num2 = 0, result = 0;
-	char ch1, ch2;
-	fp = fopen("multiple_table.txt", "wt");
-	if (fp == NULL)
+	FILE* fp;
+	BITMAPFILEHEADER bfh;
+	BITMAPINFOHEADER bih;
+	fp = fopen("img.bmp", "rb");
+	fread(&bfh, sizeof(BITMAPFILEHEADER), 1, fp);
+	if (bfh.bfType != 'MB')
 	{
-		printf("파일을 여는데 실패하였습니다.");
+		printf("비트맵 파일이 아닙니다.\n");
+		fclose(fp);
 		return 0;
 	}
-	for (int i = 1; i < 10; i++)
-		fprintf(fp, "2 * %d = %d\n", i, 2 * i);
-	fclose(fp);
-	fp = fopen("multiple_table.txt", "rt");
-	if (fp == NULL)
+	else
 	{
-		printf("파일을 여는데 실패했습니다.");
-		return 0;
+		printf("비트맵 파일이 맞습니다.\n");
 	}
-	for (int i = 1; i < 10; i++)
-	{
-		fscanf(fp, "%d %c %d %c %d", &num1, &ch1, &num2, &ch2, &result);
-		printf("%d %c %d %c %d\n", num1, ch1, num2, ch2, result);
-	}
+	printf("비트맵 타입 확인 : %c%c\n", bfh.bfType, bfh.bfType>>8);
+	printf("비트맵 파일 크기 : %.2fMB", bfh.bfSize / 1024.0f / 1024.0f);
+	fread(&bfh, sizeof(BITMAPINFOHEADER), 1, fp);
+	printf("비트맵 비트수 : %2dbit\n", bih.biBitCount);
+	printf("비트맵 넓이 : %dpixel\n", bih.biWidth);
+	printf("비트맵 높아 : %dpixel\n", bih.biHeight);
 	fclose(fp);
-	return 0;
 }
+
+// C언어 파일 입출력
+
+//typedef struct tagHERO
+//{
+//	char name[20];
+//	int hp;
+//	int mp;
+//	int att;
+//	int def;
+//	int exp;
+//}HERO;
+//
+//int main()
+//{
+//	HERO hero = { "유준헌", 200, 100, 15, 32, 3200 };
+//	HERO tmphero;
+//	FILE* fp = fopen("save.dat", "wb");
+//	fwrite((void*)&hero, sizeof(HERO), 1, fp);
+//	fclose(fp);
+//	fp = fopen("save.dat", "rb");
+//	fread((void*)&tmphero, sizeof(HERO), 1, fp);
+//	fclose(fp);
+//	printf("주인공 이름 : %s\n", tmphero.name);
+//	printf("주인공 생명력 : %d\n", tmphero.hp);
+//	printf("주인공 마법력 : %d\n", tmphero.mp);
+//	printf("주인공 공격력 : %d\n", tmphero.att);
+//	printf("주인공 방어력 : %d\n", tmphero.def);
+//	printf("주인공 경험치 : %d\n", tmphero.exp);
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+
+//typedef struct tagPERSON
+//{
+//	char name[10];
+//	char age;
+//	float weight;
+//}PERSON;
+//
+//int main()
+//{
+//	PERSON person = { 0 };
+//	PERSON tmpperson;
+//	FILE *fp;
+//	fp = fopen("person.txt", "wb+");
+//	if (fp == NULL)
+//	{
+//		printf("파일을 여는데 실패하셨습니다.");
+//		return 0;
+//	}
+//	printf("이름을 입력 하세요 : ");
+//	scanf("%s", person.name);
+//	printf("나이을 입력 하세요 : ");
+//	scanf("%d", person.age);
+//	printf("몸무게를 입력 하세요 : ");
+//	scanf("%f", person.weight);
+//	fwrite((void*)&person, sizeof(person), 1, fp);
+//	fseek(fp, 0, SEEK_SET);
+//	fread((void*)&tmpperson, sizeof(tmpperson), 1, fp);
+//	printf("이름 : %s, 나이 : %d, 몸무게 : %.2f\n", tmpperson.name, tmpperson.age, tmpperson.weight);
+//	fclose(fp);
+//	return 0;
+//}
+//int main()
+//{
+//	FILE *fp;
+//	int num1 = 0, num2 = 0, result = 0;
+//	char ch1, ch2;
+//	fp = fopen("multiple_table.txt", "wt");
+//	if (fp == NULL)
+//	{
+//		printf("파일을 여는데 실패하였습니다.");
+//		return 0;
+//	}
+//	for (int i = 1; i < 10; i++)
+//		fprintf(fp, "2 * %d = %d\n", i, 2 * i);
+//	fclose(fp);
+//	fp = fopen("multiple_table.txt", "rt");
+//	if (fp == NULL)
+//	{
+//		printf("파일을 여는데 실패했습니다.");
+//		return 0;
+//	}
+//	for (int i = 1; i < 10; i++)
+//	{
+//		fscanf(fp, "%d %c %d %c %d", &num1, &ch1, &num2, &ch2, &result);
+//		printf("%d %c %d %c %d\n", num1, ch1, num2, ch2, result);
+//	}
+//	fclose(fp);
+//	return 0;
+//}
 
 
 //int main()
