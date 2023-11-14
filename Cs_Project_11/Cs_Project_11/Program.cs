8682c8c75dc3f9fd155f55dccc6d28447ed8ef73
yujoon0214 <130519084+yujoon0214@users.noreply.github.com>
2023-11-14 20:02:07 +0900

    Add files via upload

---
+++ Cs_Project_11/Cs_Project_11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Cs_Project_11
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string str = "hello";
+            char[] chArr = new char[] { 'w', 'o', 'r', 'l', 'd' };
+            string str2 = new string(chArr);
+            string str3 = new string(chArr, 2, 3);
+            string str4 = new string('w', 4);
+            Console.WriteLine(str);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            Console.WriteLine(str4);
+            Console.WriteLine(str + str2);
+
+
+            //string str = "Hello World!";
+            
+            //Console.WriteLine(str);
+            
+            //C# 자료형
+
+            //int num1 = 10;
+            //int num2 = 20;
+            //Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+
+            //char ch = 'B';
+            //Console.WriteLine("char형의 크기 : " + sizeof(char));
+            //Console.WriteLine("char 변수 ch의 값 :" + ch);
+        }
+    }
+}
