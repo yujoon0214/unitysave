d8f85ae5a9ad07c93e0fd56032439cee09d12c6d
yujoon0214 <130519084+yujoon0214@users.noreply.github.com>
2023-11-15 19:59:46 +0900

    Add files via upload

---
+++ YJH_Test_1/YJH_Test_1/YJH_Test_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <windows.h>
+
+
+typedef struct tagPERSON
+{
+	int name[10];
+}PERSON;
+
+int main()
+{
+	tagPERSON person;
+	printf("회원가입을 시작합니다.\n");
+	printf("가입하실 이름을 입력해주세요 (한글 5, 영문 10글자 이내)\n");
+	scanf("%p", person.name);
+	printf("학생의 이름 : %d\n", person.name);
+}
